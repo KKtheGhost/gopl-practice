50e8205597c2448bda6f942de2efaac16c27510c
KKtheGhost <kivinsae@gmail.com>
2021-09-14 19:11:28 +0800

    doc(charpter4): Practice-4.4

---
+++ charpter4/rev/Practice-4.4.go
@@ -0,0 +1,17 @@
+package rev
+
+// eg. a := []int{1,2,3,4,5,6,7}, x := 5
+// length = 7
+// tmp := make([]int, 5)
+// tmp = []int{1,2,3,4,5}
+// a = []int{6,7}
+// a
+
+
+func rotate(a []int, x int) {
+	length := len(a)
+	tmp := make([]int, x)
+	copy(tmp, a[:x])
+	copy(a, a[x:])
+	copy(a[length-x:], tmp)
+}
